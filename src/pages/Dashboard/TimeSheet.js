fe8c0aa5fb45e166c0ac821198a61c1de511db27
leikezej <jezedevkiel21@gmail.com>
2022-10-13 15:33:27 +0800

    latest temporary updates

--- src/pages/Dashboard/TimeSheet.js
+++ src/pages/Dashboard/TimeSheet.js
@@ -7,40 +7,40 @@ class Table extends Component {
       super(props) 
       this.state = { 
          timesheet: [
-              {
-        image: "../../assets/user1.png",
+   {
+      //   image: "../../assets/user1.png",
         name: "Jezekiel Isip",
         start_time: "07:15:12 AM",     
-        end_time: "07:15:12 PM",
-        total_time: "8 hrs"
+        end_time: "06:15:12 PM",
+        total_time: "6.5 hrs"
     },
     {
-        image: "../../assets/user2.png",
-        name: "Jezekiel Isip",
-        start_time: "07:15:12 AM",     
-        end_time: "07:15:12 PM",
-        total_time: "8 hrs"
+      //   image: "../../assets/user2.png",
+        name: "Jaybee Geli",
+        start_time: "10:26:12 AM",     
+        end_time: "09:33:12 PM",
+        total_time: "11 hrs"
     },
     {
-        image: "../../assets/user3.png",
-        name: "Jezekiel Isip",
-        start_time: "07:15:12 AM",     
-        end_time: "07:15:12 PM",
-        total_time: "8 hrs"
+      //   image: "../../assets/user3.png",
+        name: "Mark Steven Alba",
+        start_time: "10:27:33 AM",     
+        end_time: "11:36:22 PM",
+        total_time: "12 hrs"
     },
     {
-        image: '../../assets/user4.png',
-        name: "Jezekiel Isip",
+      //   image: '../../assets/user4.png',
+        name: "Marty Pabello",
         start_time: "07:15:12 AM",     
-        end_time: "07:15:12 PM",
-        total_time: "8 hrs"
+        end_time: "08:33:56 PM",
+        total_time: "9 hrs & 28 mns"
     },
     {
-        image: "../../assets/user5.png",
-        name: "Jezekiel Isip",
-        start_time: "07:15:12 AM",     
-        end_time: "07:15:12 PM",
-        total_time: "8 hrs"
+      //   image: "../../assets/user5.png",
+        name: "Brix Vallespin",
+        start_time: "03:00:22 PM",     
+        end_time: "09:25:12 AM",
+        total_time: "10 hrs & 12 mns"
     }
          ]
       }
@@ -55,10 +55,9 @@ class Table extends Component {
 
    renderTableData() {
       return this.state.timesheet.map((timesheet, index) => {
-         const { image, name, start_time, end_time, total_time } = timesheet 
+         const { name, start_time, end_time, total_time } = timesheet 
          return (
-            <tr key={image}>
-               <td>{image}</td>
+            <tr key={name}>
                <td>{name}</td>
                <td>{start_time}</td>
                <td>{end_time}</td>
